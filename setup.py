d7aa6566505df402f3875c1eb87a111fec0b2c0c
Bernd Dorn <bernddorn@gmail.com>
2007-03-22 14:16:04 +0000

    buildout

--- setup.py
+++ setup.py
@@ -2,19 +2,23 @@
 from setuptools import setup, find_packages
 
 setup(name='z3c.configurator',
-      version='0.1',
+      version='1.1',
       author = "Zope Community",
       author_email = "zope3-dev@zope.org",
       description = "Dynamic configuration",
       license = "ZPL 2.1",
       keywords = "zope zope3",
       url='http://svn.zope.org/z3c.configurator',
-
       zip_safe=False,
       packages=find_packages('src'),
       include_package_data=True,
       package_dir = {'':'src'},
       namespace_packages=['z3c',],
-      install_requires = ['setuptools', ],
-     )
+      extras_require = dict(test=['zope.app.testing']),
+      install_requires = ['setuptools',
+                          'zope.component',
+                          'zope.schema',
+                          ],
+      dependency_links=['http://download.zope.org/distribution']
+      )
 
