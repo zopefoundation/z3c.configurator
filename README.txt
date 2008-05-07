226e5ffa4e5e6e1ba56b4d7e22d02de0a4f1b630
Roger Ineichen <roger@projekt01.ch>
2008-05-07 18:31:26 +0000

    Update pypi configuration Added README.txt and CHANGES.txt Fix configuration path for zope.app.securitypolicy and zope.app.security in ftesting.zcml
    TODO later:
    Find a way to get rid loading the hole zope trunk as dependency.
    This package offers a view and a zmi menu which includes the hole
    zope as dependency. This package clearly offers a base core
    concept and is not intend as a enhancement for rotterdam by default.

---
+++ README.txt
@@ -0,0 +1,2 @@
+This package provides a configurator which is designed to extend a component 
+after its creation for Zope3.
--- setup.py
+++ setup.py
@@ -1,21 +1,60 @@
-#!python
+##############################################################################
+#
+# Copyright (c) 2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""
+$Id:$
+"""
+
+import os
 from setuptools import setup, find_packages
 
-setup(name='z3c.configurator',
-      version='1.1.2',
-      author = "Zope Community",
-      author_email = "zope3-dev@zope.org",
-      description = "Dynamic configuration",
-      license = "ZPL 2.1",
-      keywords = "zope zope3",
-      url='http://svn.zope.org/z3c.configurator',
-      zip_safe=False,
-      packages=find_packages('src'),
-      include_package_data=True,
-      package_dir = {'':'src'},
-      namespace_packages=['z3c'],
-      extras_require = dict(test=['zope.app.testing']),
-      install_requires = [
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+    name = 'z3c.configurator',
+    version='1.1.2',
+    author = "Zope Community",
+    author_email = "zope3-dev@zope.org",
+    description = "Dynamic configuration",
+    long_description=(
+        read('README.txt')
+        + '\n\n' +
+        read('CHANGES.txt')
+        ),
+    license = 'ZPL 2.1',
+    keywords = 'zope3 z3c configurator configuration',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+    url = 'http://pypi.python.org/pypi/z3c.configurator',
+    packages = find_packages('src'),
+    include_package_data = True,
+    package_dir = {'':'src'},
+    namespace_packages = ['z3c'],
+    extras_require = dict(
+        test = [
+            'zope.app.testing',
+            ],
+        ),
+    install_requires = [
           'setuptools',
           'zope.annotation',
           'zope.app.pagetemplate',
@@ -32,6 +71,7 @@ setup(name='z3c.configurator',
           'zope.testbrowser',
           'zope.testing',
           ],
-      dependency_links=['http://download.zope.org/distribution']
-      )
+    dependency_links = ['http://download.zope.org/distribution'],
+    zip_safe=False,
+    )
 
