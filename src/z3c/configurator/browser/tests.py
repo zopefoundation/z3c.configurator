39a8cbc1f47f9ec5268a72e58d008e2fda091861
Albertas Agejevas <alga@pov.lt>
2013-03-04 11:31:01 +0200

    Made ftests optional.

--- src/z3c/configurator/browser/tests.py
+++ src/z3c/configurator/browser/tests.py
@@ -1,7 +1,14 @@
 import unittest
-from zope.app.testing import functional
 
-functional.defineLayer('TestLayer', 'ftesting.zcml')
+try:
+    from zope.app.testing import functional
+    HAVE_FTEST = True
+except ImportError:
+    HAVE_FTEST = False
+
+TestLayer = None
+if HAVE_FTEST:
+    functional.defineLayer('TestLayer', 'ftesting.zcml')
 
 
 def setUp(test):
@@ -15,14 +22,15 @@ def tearDown(test):
 
 def test_suite():
     suite = unittest.TestSuite()
-    suites = (
-        functional.FunctionalDocFileSuite('README.txt',
-                                          setUp=setUp, tearDown=tearDown,
-                                         ),
-        )
-    for s in suites:
-        s.layer=TestLayer
-        suite.addTest(s)
+    if HAVE_FTEST:
+        suites = (
+            functional.FunctionalDocFileSuite(
+                'README.txt', setUp=setUp, tearDown=tearDown,
+                ),
+            )
+        for s in suites:
+            s.layer=TestLayer
+            suite.addTest(s)
     return suite
 
 if __name__ == '__main__':
