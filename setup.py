58ecf1577c6e1005f2bc216742c8304bfe7e726d
Michael Howitz <mh@gocept.com>
2023-02-17 13:09:37 +0100

    Preparing release 3.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup(
     name='z3c.configurator',
-    version='3.0.dev0',
+    version='3.0',
     author="Zope Community",
     author_email="zope-dev@zope.dev",
     description="Dynamic configuration",
